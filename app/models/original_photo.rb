8c8f59b6e8514f58ad16414304462e3004551fe7
aiandrox <aiandrox7@gmail.com>
2024-12-22 01:33:43 +0900

    developmentでもS3を使うようにする

--- app/models/original_photo.rb
+++ app/models/original_photo.rb
@@ -3,4 +3,10 @@ class OriginalPhoto < ApplicationRecord
   has_one_attached :photo
 
   validates :photo, attached: true, processable_image: true, content_type: [ :png, :jpg, :jpeg ], size: { less_than: 10.megabytes }
+
+
+  def process_photo
+    client = Rekognition.client
+    image = Compare.new(client: client, photo: self)
+  end
 end
